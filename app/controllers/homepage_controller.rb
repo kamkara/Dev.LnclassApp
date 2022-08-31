f0c3cf77ae25505525003b731e77179e8d265881
kamkara <lnclass.kamkara@gmail.com>
2022-08-31 15:08:01 +0000

    app: Add feed

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,14 @@
 class HomepageController < ApplicationController
+  before_action :authenticatify?
+
   def index
+  end
+
+  private
+
+  def authenticatify?
+    if user_signed_in?
+      redirect_to feed_path
+    end
   end
 end
